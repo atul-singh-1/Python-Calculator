fdaeb7aea568a0d2a320eeb6a45f2b78e3152342
atul-singh-1 <atulsinghad8@gmail.com>
2026-07-17 00:20:22 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,48 @@
+while True:
+    print("\n======Calculator======")
+    print("1.Addition")
+    print("2.Subtraction")
+    print("3.Multiplication")
+    print("4.Division")
+    print("5.Modulus")
+    print("6. Exit")
+    try:
+        choice=int(input("Enter your choice :"))
+    except ValueError:
+        print(" Bro! Enter a number! Alphabet and symbol won't work here!")
+        continue
+    if choice==6:
+        print("calculator closed")
+        break
+    if choice < 1 or choice > 6:
+        print("Invalid choice! Please enter a number between 1 and 6.")
+       
+        continue
+    try:
+        a=float(input("Enter number A :"))
+        b=float(input("Enter number B :"))
+    except ValueError:
+         print("Bro! Enter a number! Alphabet and symbol won't work here!")
+         continue
+    if choice==1:
+        print("Your answer is =",a+b)
+    
+    elif choice==2:
+        print("Your answer is =",a-b)
+        
+    elif choice==3:
+        print("Your answer is =",a*b)
+
+    elif choice==4:
+        if b!=0:
+            print("Your answer is =",a/b)
+        else:
+            print("Division by zero is not possible  ")
+
+    elif choice==5:
+        if b!=0:
+            print("Your answer is =",a%b)
+        else:
+            print("Modulus of zero is not possible")    
+    else:
+        print("invalid choice !")
